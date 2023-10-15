85fe9316d2560472625ef4608e847f2d29c1253c
Biswajit Sahoo <86199974+WonTheWorld@users.noreply.github.com>
2023-10-15 12:38:56 +0530

    GameFrame.java

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -4,7 +4,7 @@ public class GameFrame extends JFrame {
 
 	GameFrame(){
 		this.add(new GamePanel());
-		this.setTitle("Biswaranjan Snake Game");
+		this.setTitle("Biswajit Snake Game");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
 		this.pack();
